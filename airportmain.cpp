9afda9c50c940d16f8cbf799a53e050a498713f4
darksonic18it <khimjayevedientes@gmail.com>
2025-09-04 20:56:36 -0700

    Update airportmain.cpp

--- airportmain.cpp
+++ airportmain.cpp
@@ -4,16 +4,18 @@ using namespace std;
 int main() {
     Airport airport;
 
+    // Predefined flights
     airport.addArrival("PAL123");
     airport.addDeparture("CEB456");
     airport.addArrival("AIR789");
-  
-    airport.displayStatus();
+    airport.addDeparture("QTR012");
 
-    airport.landPlanes();
-    airport.takeoffPlanes();
-
-    airport.displayStatus();
+    // Simulate traffic step by step
+    airport.displayStatus(); // PAL123 lands
+    airport.displayStatus(); // CEB456 takes off
+    airport.displayStatus(); // AIR789 lands
+    airport.displayStatus(); // QTR012 takes off
+    airport.displayStatus(); // nothing left
 
     return 0;
 }
