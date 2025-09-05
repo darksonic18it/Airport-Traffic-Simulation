3256e055c29a88832026e2c0163dfb80d1b55bb2
darksonic18it <khimjayevedientes@gmail.com>
2025-09-04 21:22:07 -0700

    Update airport.h

--- airport.h
+++ airport.h
@@ -2,36 +2,37 @@
 #define AIRPORT_H_INCLUDED
 
 #include <iostream>
-#include <queue>
-#include <deque>
-#include <fstream>
+#include <queue> // queue for arrivals
+#include <deque> // deque for departures
+#include <fstream> // for file management
 #include <string>
 
 using namespace std;
 
 // Airplane structure
-struct Airplane {
-    string flight;
+struct Airplane { 
+    string flight; // flight code
     string status; // Arrived, Landed, Ready for Takeoff, Took Off
 
-    Airplane(string f, string s) : flight(f), status(s) {}
+    Airplane(string f, string s) : flight(f), status(s) {} // constructor
 };
 
 class Airport {
 private:
     queue<Airplane> arrivals;   // Planes waiting to land
     deque<Airplane> departures; // Planes waiting to take off
-    ofstream logFile;
+    ofstream logFile; // file to record
 
-    void logEvent(const string& event);
+    void logEvent(const string& event); // to add text to logfile
 
 public:
-    Airport();
-    ~Airport();
+    Airport(); // cosntructor
+    ~Airport(); // destructor
 
-    void addArrival(string flight);
-    void addDeparture(string flight);
+    void addArrival(string flight); // add arrivals
+    void addDeparture(string flight); // add departures
     void displayStatus();  // now processes one plane at a time
 };
 
 #endif
+
